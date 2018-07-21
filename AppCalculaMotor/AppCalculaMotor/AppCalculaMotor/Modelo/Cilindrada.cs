c1aae7a314e990f9912653c8570c1d33aa72309c
Henrique Lazzarini <henlazza@hotmail.com>
2018-07-21 13:43:38 -0300

    Inicio do Projeto

---
+++ AppCalculaMotor/AppCalculaMotor/AppCalculaMotor/Modelo/Cilindrada.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AppCalculaMotor.Modelo
+{
+    public    class Cilindrada
+    {
+        public decimal DiametroCilintro { get; set; }
+
+        public decimal CursoPistao { get; set; }
+
+        public int NumeroCilindros { get; set; }
+    }
+}
---
+++ AppCalculaMotor/AppCalculaMotor/AppCalculaMotor/Modelo/Motor.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AppCalculaMotor.Modelo
+{
+    public class Motor
+    {
+        public int TaxaEstatica { get; set; }
+
+        public int TaxaDinamica { get; set; }
+
+        public virtual Cilindrada Cilindrada { get; set; }
+}
+
+}
